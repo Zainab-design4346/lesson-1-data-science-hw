d1e9cda9cd2564d25e206bf84cacae3ba2f94cad
Zainab-design4346 <zainabshahzad.4346@gmail.com>
2026-03-15 16:34:09 +0400

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,16 @@
+import numpy as np
+matx = np.arange(9, dtype=float64).reshape(3, 3)
+print("Matrix:")
+print(matx)
+arrA = np.array([5,5,5])
+print("\nArray:",arrA)
+
+print("Elementwise operation are as:")
+print("\naddition:")
+print(np.add(matx, arrA))
+print("\nsubtraction:")
+print(np.subtract(matx, arrA))
+print("\nmultiplication:")
+print(np.multiply(matx, arrA))
+print("\ndivision:")
+print(np.divide(matx, arrA))
